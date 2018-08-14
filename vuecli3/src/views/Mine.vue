15399d6eb677911ab058bae9eeb6044bb65b23f5
lyqqqqq <1239199841@qq.com>
2018-08-14 16:53:47 +0800

    8.14

---
+++ vuecli3/src/views/Mine.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>刘雨晴的页面</h1>
+        <router-link :to="{name:'test1',params:{name:'liuyuqing',age:20}}">子路由1</router-link> |
+        <router-link to="/mine/test2/lyq/20">子路由2</router-link>
+        <router-view></router-view>
+    </div>
+
+</template>
+<script>
+    export default {
+
+    }
+</script>
+
---
+++ vuecli3/src/views/Test2.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <h1>I'm test2</h1>
+        名字:{{$route.params.name}},
+        年龄:{{$route.params.age}}
+    </div>
+</template>
+<script>
+    export default {
+
+    }
+</script>
