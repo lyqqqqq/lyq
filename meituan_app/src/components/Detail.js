82aac17deba17b872c9e6e8e8732be5bcdc9b5ae
lyqqqqq <1239199841@qq.com>
2018-11-25 20:55:16 +0800

    meituan

---
+++ meituan_app/src/components/Detail.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+export default class Detail extends Component {
+  constructor(props) {
+    super(props);
+    
+  }
+  
+  render() {
+    return (
+      <div>
+        {/* detail {
+          this.props.match.params.id
+        } */}
+          
+      </div>
+    )
+  }
+}
